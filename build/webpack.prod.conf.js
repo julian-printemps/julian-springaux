0da2fe2c9e51e3902bf2e82a4b8e5006b22c53f6
Julian <julian.springaux@neopa.jp>
2018-03-28 19:15:51 +0900

    fix webpack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -9,6 +9,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
